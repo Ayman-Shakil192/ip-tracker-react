b8c9cd189f9b0ef87d11a355ecc3054fac1b6643
Ayman-Shakil192 <aymanshakilbms@gmail.com>
2023-03-24 15:45:16 +0530

    code refactor and bug fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { Input } from "semantic-ui-react";
-import { useMap, MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  useMap,
+  MapContainer,
+  TileLayer,
+  ZoomControl,
+  Marker,
+  Popup,
+} from "react-leaflet";
 import axios from "axios";
 import "./App.css";
 
 const App = () => {
   const [data, setData] = useState({});
-  const [searchTerm, setSearchTerm] = useState("8.8.8.8");
+  const [searchTerm, setSearchTerm] = useState("193.62.157.66");
   const [position, setPosition] = useState([51.505, -0.09]);
 
   useEffect(() => {
@@ -40,51 +47,64 @@ const App = () => {
         <div className="title">IP Address Tracker</div>
         <div className="search">
           <Input
+            className="search-bar"
             action={{
               color: "black",
-              labelPosition: "right",
               icon: "right arrow",
-              content: "Search",
               onClick: () => {
-                setSearchTerm(document.querySelector("input").value);
+                const input = document.querySelector("input").value;
+                if (input === "") {
+                  alert("Please enter an IP address or domain");
+                } else {
+                  setSearchTerm(input);
+                }
               },
             }}
             style={{
-              width: "60%",
               margin: "0 auto",
             }}
             placeholder="Search for any IP address or domain"
+            size="big"
           />
         </div>
-        <div className="ip-details-container">
-          <div className="ip-address">
-            <div className="ip-details-key">IP ADDRESS</div>
-            <div className="ip-details-value">{data.ip}</div>
-          </div>
-          <div className="hr"></div>
-          <div className="location">
-            <div className="ip-details-key">LOCATION</div>
-            <div className="ip-details-value">{`${data.location?.city}, ${data.location?.region} ${data.location?.postalCode}`}</div>
-          </div>
-          <div className="hr"></div>
-          <div className="timezone">
-            <div className="ip-details-key">TIMEZONE</div>
-            <div className="ip-details-value">{`UTC${data.location?.timezone}`}</div>
-          </div>
-          <div className="hr"></div>
-          <div className="isp">
-            <div className="ip-details-key">ISP</div>
-            <div className="ip-details-value">{data.isp}</div>
+        <div className="container">
+          <div className="ip-details-container">
+            <div className="ip-address">
+              <div className="ip-details-key">IP ADDRESS</div>
+              <div className="ip-details-value">{data.ip || "Not found"}</div>
+            </div>
+            <div className="hr"></div>
+            <div className="location">
+              <div className="ip-details-key">LOCATION</div>
+              <div className="ip-details-value">{`${
+                data.location?.city || "Not found"
+              }, ${data.location?.region || ""} ${
+                data.location?.postalCode || ""
+              }`}</div>
+            </div>
+            <div className="hr"></div>
+            <div className="timezone">
+              <div className="ip-details-key">TIMEZONE</div>
+              <div className="ip-details-value">{`UTC${
+                data.location?.timezone || "Not found"
+              }`}</div>
+            </div>
+            <div className="hr"></div>
+            <div className="isp">
+              <div className="ip-details-key">ISP</div>
+              <div className="ip-details-value">{data.isp || "Not found "}</div>
+            </div>
           </div>
         </div>
       </header>
       {position && (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+        <MapContainer center={position} zoom={13} zoomControl={false}>
           <ChangeView center={position} zoom={13} />
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          <ZoomControl position="bottomright" />
           <Marker position={position}>
             <Popup>
               {data.location?.city}, {data.location?.region}{" "}
